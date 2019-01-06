87bfdb19dc0facf3fbbad51cea66e9f215759fa2
Chih-Wei Ning <b04902079@ntu.edu.tw>
2019-01-06 20:10:35 +0800

    Modify java

--- plugins/iclue-backgroundvideo/src/android/BackgroundVideo.java
+++ plugins/iclue-backgroundvideo/src/android/BackgroundVideo.java
@@ -35,8 +35,8 @@ public class BackgroundVideo extends CordovaPlugin {
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        FILE_PATH = cordova.getActivity().getFilesDir().toString() + "/";
-        //FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).toString() + "/";
+        // FILE_PATH = cordova.getActivity().getFilesDir().toString() + "/";
+        FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).toString() + "/";
     }
 
 
@@ -114,7 +114,7 @@ public class BackgroundVideo extends CordovaPlugin {
 
                         // NOTE: GT-I9300 testing required wrapping view in relative layout for setAlpha to work.
                         RelativeLayout containerView = new RelativeLayout(cordova.getActivity());
-                        containerView.setAlpha(0.2f);
+                        containerView.setAlpha(0.01f);
                         containerView.addView(videoOverlay, new ViewGroup.LayoutParams(displaymetrics.widthPixels, displaymetrics.heightPixels));
 
                         cordova.getActivity().addContentView(containerView, new ViewGroup.LayoutParams(displaymetrics.widthPixels, displaymetrics.heightPixels));
